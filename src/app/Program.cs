2afd2be32344882c77e3ef7e55ff2935b218f26b
kedjour badr eddine <b.kedjour@outlook.com>
2019-10-13 22:32:37 +0200

    downgrade to .net 2.1

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -1,5 +1,5 @@
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 using Google.Cloud.Diagnostics.AspNetCore;
 
 namespace DogsVsCats
@@ -8,15 +8,12 @@ namespace DogsVsCats
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseGoogleDiagnostics();
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseGoogleDiagnostics()
+                .UseStartup<Startup>();
     }
 }
--- src/app/Startup.cs
+++ src/app/Startup.cs
@@ -1,11 +1,12 @@
-using DogsVsCats.Contracts;
+﻿using DogsVsCats.Contracts;
 using DogsVsCats.Models;
 using DogsVsCats.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 namespace DogsVsCats
@@ -22,7 +23,15 @@ namespace DogsVsCats
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddScoped<IDataService, DataService>();
             services.AddScoped<IImageAnalyser, ImageAnalyser>();
@@ -34,7 +43,7 @@ namespace DogsVsCats
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -43,21 +52,16 @@ namespace DogsVsCats
             else
             {
                 app.UseExceptionHandler("/Vote/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Vote}/{action=Index}/{id?}");
+                    template: "{controller=Vote}/{action=Index}/{id?}");
             });
         }
     }
